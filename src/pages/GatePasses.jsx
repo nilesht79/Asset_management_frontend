db75e77782e517610010796d25a87586c7896a9a
nilesht79 <nilesh.t@projects.polestarconsulting.in>
2026-07-07 15:52:43 +0530

    Refactor asset mapping logic in GatePasses component

--- src/pages/GatePasses.jsx
+++ src/pages/GatePasses.jsx
@@ -971,7 +971,8 @@ const GatePasses = () => {
                 </Option>
 
                 {/* Asset List */}
-                {assetSearchResults.map((asset) => (
+                /* {assetSearchResults.map((asset) => ( */
+                 {(createType === 'end_user' ? userAssets: assetSearchResults ).map((asset) => (
                   <Option key={asset.id} value={asset.id}>
                     <Space>
                       <DesktopOutlined />
